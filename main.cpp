7d559b1232d1e2162ca8c85705f75dbd2042d78e
mefikan <lev.evgenyev@yandex.ru>
2023-10-21 19:14:41 +0300

    С файлом тема пока не работает, пытаюсь починить

--- main.cpp
+++ main.cpp
@@ -72,6 +72,7 @@ int main()
     pnode root = new node();
     root = node::maketree_search(array_main,ar_size,1);
     node::init_dfs_bin_to_f(root);
+    node::print_tree_for_debugging(root,ar_size);
     pnode new_root = node::init_dfs_bin_from_f(root);
     node::print_tree_for_debugging(new_root,ar_size);
 
